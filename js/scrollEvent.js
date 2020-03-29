d2c896935aef486607bc08da9dee7c093c3bf184
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-03-29 17:43:07 +0100

    First commit

---
+++ js/scrollEvent.js
@@ -0,0 +1,47 @@
+var img1 = document.getElementById("sympooms-img-1");
+var img2 = document.getElementById("sympooms-img-2");
+var img3 = document.getElementById("sympooms-img-3");
+var img4 = document.getElementById("sympooms-img-4");
+
+var symptomsImages = document.getElementsByClassName("symptoms-img");
+
+var symptomsContent = [{
+    title: "Les premiers jours :",
+    paragraph: "La plupart ont de la fièvre. Le virus peut également provoquer des douleurs musculaires, de la fatigue, des  étourdissements et même de la diarrhée. La plupart montrent des signes d’amélioration après 6 à 7 jours.",
+}, {
+    title: "Aux premiers stades de la maladie:",
+    paragraph: "Les symptômes courants seront les maux de tête, la toux sèche et le nez qui coule. Finalement, si la maladie s’aggrave, le patient peut avoir une toux grasse ou même une toux sanglante.",
+}, {
+    title: "Complications :",
+    paragraph: "Une respiration difficile et / ou des douleurs thoraciques. Une insuffisance pulmonaire aiguë ou un essoufflement nécessitant un apport d’oxygène peut survenir dans les pires cas. Les symptômes surviennent généralement vers le cinquième jour de la maladie.",
+}, {
+    title: "Dans certains cas :",
+    paragraph: "Le coronavirus entraîne une insuffisance rénale. Les symptômes comprennent des visites fréquentes aux toilettes, un gonflement des jambes et sous les yeux, une pression artérielle élevée et / ou une fatigue et une faiblesse."
+}
+];
+
+window.onscroll = () => {
+    symptomsImages.forEach(image => {
+        if (image.getBoundingClientRect().top <= window.outerHeight/2 && 0 <= image.getBoundingClientRect().top) {
+            image.style.opacity = 1;
+                changeSymptomsContent(image);
+        } else {
+            image.style.opacity = 0;
+        }
+        // if (image.getBoundingClientRect().bottom <= window.outerHeight/2) {
+        //     changeAuthorised = true;
+        //     image.style.opacity = 0;
+        // }
+    });
+};
+
+function changeSymptomsContent(e) {
+    var arr = [].slice.call(symptomsImages);
+    title = document.getElementsByClassName("symptoms-title")[0];
+    paragraph = document.getElementsByClassName("symptoms-paragraph")[0];
+
+    title.innerHTML = symptomsContent[arr.indexOf(e)].title;
+    paragraph.innerHTML = symptomsContent[arr.indexOf(e)].paragraph;
+
+}
+
