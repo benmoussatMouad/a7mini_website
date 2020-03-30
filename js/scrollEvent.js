e9fd08318d5fc99d692d7a9340f926b46d350c04
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-03-30 21:48:27 +0100

    Added Symptoms section and FAQ section.

--- js/scrollEvent.js
+++ js/scrollEvent.js
@@ -1,8 +1,4 @@
-var img1 = document.getElementById("sympooms-img-1");
-var img2 = document.getElementById("sympooms-img-2");
-var img3 = document.getElementById("sympooms-img-3");
-var img4 = document.getElementById("sympooms-img-4");
-
+/*Symptoms Section Scrollevent handling */
 var symptomsImages = document.getElementsByClassName("symptoms-img");
 
 var symptomsContent = [{
@@ -20,20 +16,6 @@ var symptomsContent = [{
 }
 ];
 
-window.onscroll = () => {
-    symptomsImages.forEach(image => {
-        if (image.getBoundingClientRect().top <= window.outerHeight/2 && 0 <= image.getBoundingClientRect().top) {
-            image.style.opacity = 1;
-                changeSymptomsContent(image);
-        } else {
-            image.style.opacity = 0;
-        }
-        // if (image.getBoundingClientRect().bottom <= window.outerHeight/2) {
-        //     changeAuthorised = true;
-        //     image.style.opacity = 0;
-        // }
-    });
-};
 
 function changeSymptomsContent(e) {
     var arr = [].slice.call(symptomsImages);
@@ -43,5 +25,42 @@ function changeSymptomsContent(e) {
     title.innerHTML = symptomsContent[arr.indexOf(e)].title;
     paragraph.innerHTML = symptomsContent[arr.indexOf(e)].paragraph;
 
+    // Showing Button, or not
+    var btn= document.getElementById("symptoms-test-btn");
+    if (arr.indexOf(e) === 3) {
+        // btn.style.display = "block";
+        btn.style.width = "auto";
+        btn.style.height = "auto";
+        btn.style.padding = "8px 25px";
+        setTimeout(1000);
+        btn.style.opacity = "1";
+    } else {
+        btn.style.opacity = 0;
+        btn.style.width = "0";
+        btn.style.height = "0";
+        btn.style.padding = "0  ";
+    }
+
 }
+/***************************************/
+
+
+
+window.onscroll = () => {
+    symptomsImages.forEach(image => {
+        if (image.getBoundingClientRect().top <= window.outerHeight/2 && 0 <= image.getBoundingClientRect().top) {
+            image.style.opacity = 1;
+                changeSymptomsContent(image);
+        } else {
+            image.style.opacity = 0;
+            if (image.id == "symptoms-img-4" && (image.getBoundingClientRect().bottom <= window.outerHeight / 2 || image.getBoundingClientRect().top <= 0)) {
+                image.style.opacity = 1;
+            }
+        }
+        // if (image.getBoundingClientRect().bottom <= window.outerHeight/2) {
+        //     changeAuthorised = true;
+        //     image.style.opacity = 0;
+        // }
+    });
+};
 
