51e76ab2d700c557844d4860deb58620c0879536
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-04-04 20:45:06 +0100

    Fixed many issues with mobile version. Added the footer. Status bar. CountUp animation. Included the map and charts from piktochart.

--- js/header.js
+++ js/header.js
@@ -9,3 +9,28 @@ window.addEventListener("scroll", () => {
     headerBg.style.opacity = window.scrollY / height ;
 
 });
+
+
+// Mobile Menu Script
+function openMobileMenu() {
+    var mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+    var burgerBtn = document.getElementsByClassName("btn-burger")[0];
+    var closeBtn = document.getElementsByClassName("btn-close")[0];
+
+    mobileMenu.classList.remove("closed-menu");
+    mobileMenu.classList.add("open-menu");
+    burgerBtn.style.display = "none";
+    closeBtn.style.display = "block";
+}
+
+function closeMobileMenu() {
+    var mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+    var burgerBtn = document.getElementsByClassName("btn-burger")[0];
+    var closeBtn = document.getElementsByClassName("btn-close")[0];
+    mobileMenu.classList.add("closed-menu");
+    mobileMenu.classList.remove("open-menu");
+    burgerBtn.style.display = "block";
+    closeBtn.style.display = "none";
+}
+
+// **************************
