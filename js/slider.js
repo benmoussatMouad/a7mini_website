fb71d411b6ee429a952e7ce757e9c51906ea63c9
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-05-13 19:26:53 +0100

    Corrected some bugs related to the animation.

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,7 @@ var sliders = document.getElementsByClassName("slider__nav");
 var sliderArray = [].slice.call(sliders) //changing HTML collection to array, for manipulation
 var lenght =sliderArray.length;
 
-setInterval(() => {
-    counter = (counter + 1) % lenght;
-    sliderArray[counter].checked = true;
-}, 4500);
+// setInterval(() => {
+//     counter = (counter + 1) % lenght;
+//     sliderArray[counter].checked = true;
+// }, 4500);
