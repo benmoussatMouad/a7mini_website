8a6a2633e53104b6a51b25c5762661d8e5927b6c
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-06-25 19:33:59 +0100

    All recommendations are done. profess recomends need arabic version

--- public/js/recommendations.js
+++ public/js/recommendations.js
@@ -1,24 +1,32 @@
-var recommendationsElements = document.getElementsByClassName("recommendation");
-var recoArray = [].slice.call(recommendationsElements);
-var isOpen = [];
+function setRecommendationsAnimations() {
+    var recommendationsElements = document.getElementsByClassName("recommendation");
+    var recoArray = [].slice.call(recommendationsElements);
+    var isOpen = [];
 
-for (i = 0; i < recoArray.length; i++) {
-    isOpen.push(false);
+    for (i = 0; i < recoArray.length; i++) {
+        isOpen.push(false);
+    }
+
+    recoArray.forEach((recommendation, i) => {
+        var recoTitle = recommendation.getElementsByClassName("recommendation-title")[0];
+        var recoContent = recommendation.getElementsByClassName("recommendation-content")[0];
+        var height = recoContent.offsetHeight;
+        recoContent.style.maxHeight = "0px";
+
+        recoTitle.onclick = () => {
+
+            if (!isOpen[i]) {
+                recoContent.style.maxHeight = height + "px";
+                isOpen[i] = true;
+                setTimeout(() => {
+                    recoContent.style.maxHeight = "none";
+                }, 1200);
+
+            } else {
+                recoContent.style.maxHeight = "0px";
+                isOpen[i] = false
+            }
+        };
+    });
 }
-
-recoArray.forEach((recommendation,i) => {
-    var recoTitle = recommendation.getElementsByClassName("recommendation-title")[0];
-    var recoContent = recommendation.getElementsByClassName("recommendation-content")[0];
-    var height = recoContent.offsetHeight;
-    recoContent.style.maxHeight = "0px";
-
-    recoTitle.onclick = () => {
-        if (!isOpen[i]) {
-            recoContent.style.maxHeight = height + "px";
-            isOpen[i] = true;
-        } else {
-            recoContent.style.maxHeight = "0px";
-            isOpen[i] = false
-        }
-    };
-});
+setRecommendationsAnimations();
