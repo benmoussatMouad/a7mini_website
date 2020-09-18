51d8e4972f772c3d769abf2ca37ee93e6ef0b499
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-09-18 21:05:05 +0100

    Fixing the "no render engine" issue

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('/public/index.html', {root: '/'});
+  res.sendFile('/index.html', {root: 'public'});
 });
 
 module.exports = router;
