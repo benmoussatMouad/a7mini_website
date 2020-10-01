9b22254a920aa2df10ffdf3052d9b54b686c5682
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-10-01 17:23:55 +0100

    Fixing the path not found issue

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', indexRouter);
-
+console.log('getData');
 app.use('/getData', dataRouter);
 
 app.get("/google-spreadsheet", async function(req, res){
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("HELLO");
-  res.send(__dirname);;
+  res.send(__dirname.slice(0 , __dirname.length - 6));;
 });
+
+router.get('/js/loader.js', (req, res, next) => {
+  res.sendFile('/public/js/loader.js', {root: __dirname.slice(0 , __dirname.length - 6)} );;
+});
+
 
 module.exports = router;
